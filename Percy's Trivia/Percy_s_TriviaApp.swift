86c4f8a8e944a19c4ea062dd6603cb3e0aab7fd6
Hirav T <hirav.a.s.thakur@uwaterloo.ca>
2023-01-08 19:49:01 -0500

    Initial Commit

---
+++ Percy's Trivia/Percy_s_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Percy_s_TriviaApp.swift
+//  Percy's Trivia
+//
+//  Created by Hirav Thakur on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Percy_s_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
